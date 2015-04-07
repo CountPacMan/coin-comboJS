02e020ba6085b3de4cb992d5c5ba1c802ca07d2b
kelly de Vries <kdv@me.com>
2015-04-07 16:49:14 -0700

    Update string output to deal with plurals/singulars

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,76 @@
 function coinCombo(n) {
   n *= 100;
 
-  var coins = ["half-dollars", "quarters", "dimes", "nickels", "pennies"];
+  // var coins = ["half-dollars", "quarters", "dimes", "nickels", "pennies"];
 
   var tally = [];
 
   if (n > 0) {
     if (n >= 50) {
       var half_dollars = Math.floor(n / 50);
-
-      tally.push(half_dollars);
+      if (half_dollars === 1) {
+        tally.push("1 half-dollar");
+      } else if (half_dollars > 1) {
+        tally.push(half_dollars + " half-dollars");
+      } else {
+        tally.push(0);
+      }
       n %= 50;
     } else {
       tally.push(0);
     }
+
     if (n >= 25) {
       var quarters = Math.floor(n / 25);
-      tally.push(quarters);
+      if (quarters === 1) {
+        tally.push("1 quarter");
+      } else if (quarters > 1) {
+        tally.push(quarters + " quarters");
+      } else {
+        tally.push(0);
+      }
       n %= 25;
-    }else {
+    } else {
       tally.push(0);
     }
+
     if (n >= 10) {
       var dimes = Math.floor(n / 10);
-      tally.push(dimes);
+      if (dimes === 1) {
+        tally.push("1 dime");
+      } else if (dimes > 1) {
+        tally.push(dimes + " dimes");
+      } else {
+        tally.push(0);
+      }
       n %= 10;
-    }else {
+    } else {
       tally.push(0);
     }
+
     if (n >= 5) {
       var nickels = Math.floor(n / 5);
-      tally.push(nickels);
+      if (nickels === 1) {
+        tally.push("1 nickel");
+      } else if (nickels > 1) {
+        tally.push(nickels + " nickels");
+      } else {
+        tally.push(0);
+      }
       n %= 5;
-    }else {
+    } else {
       tally.push(0);
     }
+
     if (n >= 1) {
       var pennies = Math.floor(n);
-
-      tally.push(pennies);
+      if (pennies === 1) {
+        tally.push("1 penny");
+      } else if (pennies > 1) {
+        tally.push(pennies + " pennies");
+      } else {
+        tally.push(0);
+      }
       n = 0;
     }
   }
@@ -46,8 +78,8 @@ function coinCombo(n) {
   var result = "Your coin combo is ";
 
   for (var i = 0; i <= 4; i++) {
-    if(tally[i] > 0) {
-      result += tally[i] + " " + coins[i] + ", ";
+    if(tally[i] !== 0) {
+      result += tally[i] + ", ";
     }
   }
 
