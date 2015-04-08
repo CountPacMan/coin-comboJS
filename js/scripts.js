855dc4e699dc1fde4906fad3a7d638dc3ba9c132
Daniel Toader & Cynthia Wilhelm <student@epicodus.com>
2015-04-08 10:54:37 -0700

    refactor code

--- js/scripts.js
+++ js/scripts.js
@@ -1,89 +1,43 @@
 function coinCombo(n) {
   n *= 100;
 
-  // var coins = ["half-dollars", "quarters", "dimes", "nickels", "pennies"];
+  var tally = {"half-dollar": 0, "quarter": 0, "dime": 0, "nickel": 0, "penny": 0};
 
-  var tally = [];
-
-  if (n > 0) {
-    if (n >= 50) {
-      var half_dollars = Math.floor(n / 50);
-      if (half_dollars === 1) {
-        tally.push("1 half-dollar");
-      } else if (half_dollars > 1) {
-        tally.push(half_dollars + " half-dollars");
-      } else {
-        tally.push(0);
-      }
-      n %= 50;
+  while (n > 0) {
+    if (n < 5) {
+      tally["penny"]++;
+      n--;
+    } else if (n < 10) {
+      tally["nickel"]++;
+      n -= 5;
+    } else if (n < 25) {
+      tally["dime"]++;
+      n -= 10;
+    } else if (n < 50) {
+      tally["quarter"]++;
+      n -= 25;
     } else {
-      tally.push(0);
-    }
-
-    if (n >= 25) {
-      var quarters = Math.floor(n / 25);
-      if (quarters === 1) {
-        tally.push("1 quarter");
-      } else if (quarters > 1) {
-        tally.push(quarters + " quarters");
-      } else {
-        tally.push(0);
-      }
-      n %= 25;
-    } else {
-      tally.push(0);
-    }
-
-    if (n >= 10) {
-      var dimes = Math.floor(n / 10);
-      if (dimes === 1) {
-        tally.push("1 dime");
-      } else if (dimes > 1) {
-        tally.push(dimes + " dimes");
-      } else {
-        tally.push(0);
-      }
-      n %= 10;
-    } else {
-      tally.push(0);
-    }
-
-    if (n >= 5) {
-      var nickels = Math.floor(n / 5);
-      if (nickels === 1) {
-        tally.push("1 nickel");
-      } else if (nickels > 1) {
-        tally.push(nickels + " nickels");
-      } else {
-        tally.push(0);
-      }
-      n %= 5;
-    } else {
-      tally.push(0);
-    }
-
-    if (n >= 1) {
-      var pennies = Math.floor(n);
-      if (pennies === 1) {
-        tally.push("1 penny");
-      } else if (pennies > 1) {
-        tally.push(pennies + " pennies");
-      } else {
-        tally.push(0);
-      }
-      n = 0;
+      tally["half-dollar"]++;
+      n -= 50;
     }
   }
 
   var result = "Your coin combo is ";
 
-  for (var i = 0; i <= 4; i++) {
-    if(tally[i] !== 0) {
-      result += tally[i] + ", ";
+  $.each(tally, function(key, value) {
+    if (key === "penny" && value > 1) {
+      key = "pennie";
     }
-  }
+    if (value > 1) {
+      result += value + " " + key + "s, ";
+    } else if (value > 0) {
+      result += value + " " + key + ", ";
+    }
+  });
 
+  // remove extraneous comma and space at end
   result = result.substring(0, result.length - 2);
+  
   return result;
 };
 
